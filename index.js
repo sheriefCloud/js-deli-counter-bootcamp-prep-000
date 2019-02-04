9e5d0b5f56f8b90006f5788b179034aa7257c172
Sherief Farid <sherief@farid.cloud>
2019-02-04 02:39:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@
 function takeANumber (katzDeli,name){
   [...katzDeli, name];
   var position = katzDeli.length
-  var message = "Welcome, " +name+" You are number "+ (position+1) +" in line."
+  var message = "Welcome, " +name+" You are number "+ position +" in line."
   return message
 }
